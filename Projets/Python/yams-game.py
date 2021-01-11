e3275e02196fea9b03fcb861ab42aced2b9fcb88
kevinniel <k.niel.pro@gmail.com>
2021-01-11 10:56:06 +0100

    ajout projet python

---
+++ Projets/Python/yams-game.py
@@ -0,0 +1,23 @@
+# Jeu du YAMS
+
+## Liens utiles
+
+- https://www.regles-de-jeux.com/regle-du-yams/
+- https://doc.qt.io/
+- https://dev.mysql.com/doc/connector-python/en/
+
+## Énoncé
+
+Dans le but de réaliser le jeu "YAMS", vous devrez développer une interface graphique grâce à PyQT.
+
+Vous devrez donc commencer par étudier les règles du jeu, pour savoir comment il fonctionne et ainsi pouvoir créer l'algorythme correspondant.
+
+Vous devrez ensuite interfacer cet algorythme avec une interface graphique, générée grâce à la librairie PyQT. (dernière version)
+
+Vous pourrez enfin mettre en place une base de données, afin de sauvegarder l'avancement d'une partie à tout moment. Vous devrez également permettre l'enregistrement des joueurs afin d'obtenir un listing de tous ceux ayant joué sur votre programme.
+
+
+## Contraintes
+
+- Respect de la PEP8 avec __Pylint__ ET __Flake8__
+- commits réguliers avec des bons messages, clairs et explicites
